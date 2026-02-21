062b08006675f2c8b971202e2868ce82c2b09392
kaiqui <kaiqui82@gmail.com>
2026-02-21 18:15:39 -0300

    chore: update src/application/ports/status_writer.py

---
+++ src/application/ports/status_writer.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+from typing import Dict, Any
+
+
+class StatusWriter(ABC):
+
+    @abstractmethod
+    def update(
+        self,
+        body: Dict[str, Any],
+        status: Dict[str, Any]
+    ) -> None:
+        pass
