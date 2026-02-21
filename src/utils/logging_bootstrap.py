9f280559c26dcb58c89210ac2249f2ca3506306e
kaiqui <kaiqui82@gmail.com>
2026-02-21 18:15:40 -0300

    chore: update src/utils/logging_bootstrap.py

---
+++ src/utils/logging_bootstrap.py
@@ -0,0 +1,10 @@
+from src.utils.json_logger import setup_logger
+
+def init_logging():
+    # Root do operador
+    setup_logger("controller", level="INFO")
+    setup_logger("SLOService", level="INFO")
+    setup_logger("DatadogRepository", level="INFO")
+    setup_logger("SLOManager", level="INFO")
+    setup_logger("DeploymentsController", level="INFO")
+    setup_logger("ServiceController", level="INFO")
