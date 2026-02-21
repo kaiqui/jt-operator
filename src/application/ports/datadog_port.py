23a7ea0a13c2aefe4bd269e280782c04e799d35b
kaiqui <kaiqui82@gmail.com>
2026-02-21 18:15:39 -0300

    chore: update src/application/ports/datadog_port.py

---
+++ src/application/ports/datadog_port.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+from typing import List, Optional
+from src.domain.models import ServiceDefinition, SLO
+
+
+class DatadogPort(ABC):
+    
+    
+    @abstractmethod
+    def get_service_definition(self, service_name: str) -> Optional[ServiceDefinition]:
+        
+        pass
+    
+    @abstractmethod
+    def get_service_slos(self, service_name: str) -> List[SLO]:
+        
+        pass
+    
+    @abstractmethod
+    def create_slo(self, slo: SLO) -> Optional[str]:
+        
+        pass
+    
+    @abstractmethod
+    def update_slo_apps(self, slo_id: str, slo: SLO) -> bool:
+        
+        pass
